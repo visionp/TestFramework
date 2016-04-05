cf7befb7c723eb24aa5557c01caef9dde640ad95
vision <visionp1986@gmail.com>
2016-04-05 17:20:25 +0300

    config

--- app/_config.php
+++ app/_config.php
@@ -7,11 +7,18 @@
  */
 return [
     'components' => [
-        'db' => [
+        'db_pdo' => [
             'class' => 'app\components\Pdo',
             'user' => 'root',
             'password' => '',
             'dbname' => ''
-        ]
+        ],
+        'db' => [
+            'class' => 'app\components\Doctrine',
+            'host' => 'localhost',
+            'user' => 'root',
+            'password' => '',
+            'dbname' => ''
+        ],
     ]
 ];
